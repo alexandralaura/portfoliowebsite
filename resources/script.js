4baeb56484f867c0ff59f3af48b4aec60710bb6c
Alexandra Irwin <airwin357@gmail.com>
2023-11-19 08:55:56 +1100

    sunday19thcommit

--- resources/script.js
+++ resources/script.js
@@ -7,3 +7,15 @@ let eventTarget2 = document.getElementById('secondturtle');
 };
 //eventTarget.addEventListener('click', picturechange);
 eventTarget.onclick = picturechange;
+let howdy = document.getElementById('getthis');
+function howdychange() {
+    //howdy.style.transform = 'rotate(45)';
+    howdy.style.backgroundColor = 'purple';
+}
+howdy.addEventListener('click', howdychange);
+let paragraph = document.getElementById('helloworld');
+function hellochange () {
+    paragraph.style.fontFamily = "helvetica";
+    paragraph.style.lineHeight = "2";
+}
+paragraph.addEventListener('mouseover', hellochange);
